7dde05f4bdd6e07ae397714b9a4bc4e531eb0396
Weltraumschaf <ich@weltraumschaf.de>
2011-10-04 18:16:29 +0200

    Add concat and exception operator.

--- tests/ScannerTest.php
+++ tests/ScannerTest.php
@@ -26,7 +26,7 @@ require_once "Scanner.php";
  */
 class ScannerTest extends \PHPUnit_Framework_TestCase {
 
-    private $ops = array("(", ")", "[", "]", "{", "}", "=", ".", ";", "|");
+    private $ops = array("(", ")", "[", "]", "{", "}", "=", ".", ";", "|", ",", "-");
     private $lowAlpha = array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
         "s", "t", "u", "v", "w", "x", "y", "z");
     private $upAlpha = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
