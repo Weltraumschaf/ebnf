226e973a9c7c580ec90baa61983aa110b31cf65f
Weltraumschaf <ich@weltraumschaf.de>
2011-12-10 10:01:11 +0100

    Add documentation and tests.

--- src/EBNF/ast/Composite.php
+++ src/EBNF/ast/Composite.php
@@ -1,9 +1,35 @@
 <?php
 
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/ GNU General Public License
+ * @author  Sven Strittmatter <ich@weltraumschaf.de>
+ * @package ast
+ */
+
 namespace de\weltraumschaf\ebnf\ast;
 
 use \IteratorAggregate;
 
+/**
+ * Represents an object in the AST model which can have
+ * some child {@link Node nodes}.
+ *
+ * @package ast
+ * @version @@version@@
+ */
 interface Composite extends IteratorAggregate {
 
     /**
@@ -28,4 +54,5 @@ interface Composite extends IteratorAggregate {
      * @return void
      */
     public function addChild(Node $child);
+    
 }
