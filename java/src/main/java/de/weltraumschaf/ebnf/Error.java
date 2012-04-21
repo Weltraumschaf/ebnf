fed2a66ecd183ff091fbd884df7adae44e2b5d72
Weltraumschaf <ich@weltraumschaf.de>
2012-04-21 19:15:47 +0200

    Add generic error class.

---
+++ java/src/main/java/de/weltraumschaf/ebnf/Error.java
@@ -0,0 +1,59 @@
+package de.weltraumschaf.ebnf;
+
+/**
+ * Represents an  error which provides an error code.
+ *
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+public class Error extends Exception {
+
+    /**
+     * The error code.
+     *
+     * By default this is -1.
+     */
+    private int code;
+
+    /**
+     * Initializes with error message.
+     *
+     * Initializes code with -1.
+     *
+     * @param message Error message.
+     */
+    public Error(String message) {
+        this(message, -1);
+    }
+
+    /**
+     * Initializes with error message and code.
+     *
+     * @param message Error message.
+     * @param code    Error code.
+     */
+    public Error(String message, int code) {
+        this(message, code, null);
+    }
+
+    /**
+     * Dedicated constructor.
+     *
+     * @param message Error message.
+     * @param code    Error code.
+     * @param cause   Previous errors.
+     */
+    public Error(String message, int code, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+    }
+
+    /**
+     * Returns the error code.
+     *
+     * @return
+     */
+    public int getCode() {
+        return code;
+    }
+
+}
