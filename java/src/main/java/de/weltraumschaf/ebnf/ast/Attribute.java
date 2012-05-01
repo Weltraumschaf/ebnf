297445494425dc6327917c7217d72fd6337c9677
Weltraumschaf <ich@weltraumschaf.de>
2012-05-01 13:47:42 +0200

    Add attribute annotation for nodes.

---
+++ java/src/main/java/de/weltraumschaf/ebnf/ast/Attribute.java
@@ -0,0 +1,13 @@
+package de.weltraumschaf.ebnf.ast;
+
+import java.lang.annotation.*;
+
+/**
+ * Indicates an AST {@link Node} attribute.
+ * 
+ * @author Sven Strittmatter <weltraumschaf@googlemail.com>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Documented
+public @interface Attribute {}
